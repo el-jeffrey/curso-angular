017a59c14d71313a9cb3c1826afce0092bec4509
jespinel <jespinelp.d@outlook.com>
2020-04-22 18:55:23 -0500

    clase_2.2 Arrow Functions

--- app.js
+++ app.js
@@ -1,56 +1,45 @@
 "use strict";
 (function () {
-    //Tipos de Parámetros:
-    //Obligatorios, Opcionales se declaran (?:tipo_de_dato), Por Defectos
-    /*function suma(a: number, c: number = 5, b?: number): number {
-        if (b) {
-            return a + b + c;
-        }
-        return a + c;
-    }
-
-    console.log(suma(1,2));
-    */
-    //Función con nombre
-    /*
-    function picking(cantidad: number, posicion?: string, msu: string = 'MJ') {
-        return `Robots pickeando ${cantidad} productos en la MSU ${msu}, en la posición ${posicion}`;
-    }
-    console.log(picking(4, 'A5'));
-    */
-    //Funciones Anónimas Sintaxis
-    /*var x = function (a: number, b: number) {
-        return a + b;
-    }
-    console.log(x(4, 7));*/
-    /*
-        
-        //Función con nombre
-        function saludar() {
-            console.log(mensaje())
-            function mensaje() {
-                return otraFuncion();
-                function otraFuncion(){
-                    return 'Jeffrey'
-                }
-            }
-        }
-    
-        //Función con nombre
-        function saludarAnom() {
-            var mensaje = function () {
-                return 'Jeffrey'
-            }
-            console.log(mensaje())
-        }
-    
-        saludar();
-        saludarAnom();
-    */
     /** Ejercicio 1
      * a=2;
      * b=3;
      * res = (2*2) + (3*3);
      * res= 4+9 =13;
      */
+    var sumaCuadrados = function (a, b) {
+        var cuadrado = function (valor) { return valor * valor; };
+        return (cuadrado(a) + cuadrado(b));
+    };
+    console.log("El cuadrado es: " + sumaCuadrados(5, 8));
+    /*
+   function saludar(destinatario: string): void {
+       console.log(`Hola ${destinatario}`)
+   }
+
+   var saludarArrow=(destinatario:string)=>{
+       console.log(`Hola ${destinatario}`);
+   }
+
+   saludar('Oscar');
+   saludarArrow('OscarArrow');
+   */
+    var suma = function (a, b) { return a + b; };
+    function sumaNormal(a, b) {
+        return a + b;
+    }
+    console.log(suma(1, 2));
+    console.log(sumaNormal(1, 2));
+    var SKU = {
+        nombre: 'Taladro',
+        saludar: function () {
+            var _this = this;
+            setTimeout(function name() {
+                console.log("Hola, soy el sku " + this.nombre);
+            }, 200);
+            setTimeout(function () {
+                console.log("Hola flecha, soy el sku " + _this.nombre);
+            }, 1000);
+        }
+    };
+    SKU.saludar();
 })();
--- app.ts
+++ app.ts
@@ -1,57 +1,5 @@
 (function () {
 
-    //Tipos de Parámetros:
-    //Obligatorios, Opcionales se declaran (?:tipo_de_dato), Por Defectos
-
-    /*function suma(a: number, c: number = 5, b?: number): number {
-        if (b) {
-            return a + b + c;
-        }
-        return a + c;
-    }
-
-    console.log(suma(1,2));
-    */
-
-    //Función con nombre
-    /*
-    function picking(cantidad: number, posicion?: string, msu: string = 'MJ') {
-        return `Robots pickeando ${cantidad} productos en la MSU ${msu}, en la posición ${posicion}`;
-    }
-    console.log(picking(4, 'A5'));
-    */
-
-    //Funciones Anónimas Sintaxis
-    /*var x = function (a: number, b: number) {
-        return a + b;
-    }
-    console.log(x(4, 7));*/
-
-    /*
-        
-        //Función con nombre
-        function saludar() {
-            console.log(mensaje())
-            function mensaje() {
-                return otraFuncion();
-                function otraFuncion(){
-                    return 'Jeffrey'
-                }
-            }
-        }
-    
-        //Función con nombre
-        function saludarAnom() {
-            var mensaje = function () {
-                return 'Jeffrey'
-            }
-            console.log(mensaje())        
-        }
-    
-        saludar();
-        saludarAnom();
-    */
-
     /** Ejercicio 1
      * a=2;
      * b=3;
@@ -59,5 +7,49 @@
      * res= 4+9 =13;
      */
 
+
+    var sumaCuadrados = (a: number, b: number) => {
+        var cuadrado = (valor: number) => valor * valor;
+        return (cuadrado(a) + cuadrado(b));
+    }
+
+    console.log(`El cuadrado es: ${sumaCuadrados(5, 8)}`)
+
+
+    /*
+   function saludar(destinatario: string): void {
+       console.log(`Hola ${destinatario}`)
+   }
+
+   var saludarArrow=(destinatario:string)=>{
+       console.log(`Hola ${destinatario}`);
+   }
+
+   saludar('Oscar');
+   saludarArrow('OscarArrow');
+   */
+
+    var suma = (a: number, b: number) => a + b;
+
+    function sumaNormal(a: number, b: number) {
+        return a + b;
+    }
+
+    console.log(suma(1, 2));
+    console.log(sumaNormal(1, 2));
+
+    const SKU: any = {
+        nombre: 'Taladro',
+        saludar() {
+            setTimeout(function name() {
+                console.log(`Hola, soy el sku ${this.nombre}`)
+            }, 200);
+            setTimeout(() => {
+                console.log(`Hola flecha, soy el sku ${this.nombre}`)
+            }, 1000);
+        }
+    }
+
+    SKU.saludar();
 
 })();
